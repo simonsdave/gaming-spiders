9a21e36645066fe35eb5a51890b82449b9cbe5ad
Dave Simons <simonsdave@gmail.com>
2015-06-08 17:12:45 -0400

    first real commit for repo getting basics setup

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gaming_spiders
+A collection of [CloudFeaster](https://github.com/simonsdave/clf)
+spiders for various loyalty programs.
+
+##Getting Started
+
+```bash
+>cd
+>git clone https://github.com/simonsdave/gaming_spiders.git
+Cloning into 'gaming_spiders'...
+remote: Counting objects: 142, done.
+remote: Compressing objects: 100% (61/61), done.
+remote: Total 142 (delta 85), reused 128 (delta 76), pack-reused 0
+Receiving objects: 100% (142/142), 24.33 KiB | 0 bytes/s, done.
+Resolving deltas: 100% (85/85), done.
+Checking connectivity... done.
+>cd gaming_spiders/
+>source cfg4dev
+<<<snip lots>>>
+(env)>
+```
---
+++ setup.py
@@ -0,0 +1,55 @@
+#
+# to build the distrubution @ dist/gaming_spiders-*.*.*.tar.gz
+#
+#   >git clone https://github.com/simonsdave/gaming_spiders.git
+#   >cd gaming_spiders
+#   >python setup.py sdist --formats=gztar
+#
+
+import re
+
+from setuptools import setup
+
+#
+# this approach used below to determine ```version``` was inspired by
+# https://github.com/kennethreitz/requests/blob/master/setup.py#L31
+#
+# why this complexity? wanted version number to be available in the
+# a runtime.
+#
+# the code below assumes the distribution is being built with the
+# current directory being the directory in which setup.py is stored
+# which should be totally fine 99.9% of the time. not going to add
+# the coode complexity to deal with other scenarios
+#
+reg_ex_pattern = r"__version__\s*=\s*['\"](?P<version>[^'\"]*)['\"]"
+reg_ex = re.compile(reg_ex_pattern)
+version = ""
+with open("gaming_spiders/__init__.py", "r") as fd:
+    for line in fd:
+        match = reg_ex.match(line)
+        if match:
+            version = match.group("version")
+            break
+if not version:
+    raise Exception("Can't locate spiders' version number")
+
+setup(
+    name="gaming_spiders",
+    packages=[
+        "gaming_spiders",
+    ],
+    scripts=[
+    ],
+    install_requires=[
+        "cloudfeaster==0.5.0",
+    ],
+    dependency_links=[
+        "http://github.com/simonsdave/cloudfeaster/tarball/master#egg=cloudfeaster-0.5.0",
+    ],
+    version=version,
+    description="Gaming Spiders",
+    author="Dave Simons",
+    author_email="simonsdave@gmail.com",
+    url="https://github.com/simonsdave/gaming_spiders"
+)
