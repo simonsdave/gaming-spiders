50c709183af4d9bc3f978b36513819cd34b481b3
Dave Simons <simonsdave@gmail.com>
2015-11-22 10:37:21 -0500

    refactor zygomatic spiders

--- gaming_spiders/hiddenobjectgames.py
+++ gaming_spiders/hiddenobjectgames.py
@@ -3,10 +3,11 @@
 import json
 
 from cloudfeaster import spider
-from cloudfeaster import webdriver_spider
+
+from zygomatic import ZygomaticSpider
 
 
-class HiddenObjectGamesSpider(spider.Spider):
+class HiddenObjectGamesSpider(ZygomaticSpider):
 
     @classmethod
     def get_metadata(cls):
@@ -14,29 +15,6 @@ class HiddenObjectGamesSpider(spider.Spider):
             "url": "http://www.hiddenobjectgames.com/?sort=mostPlayed",
         }
 
-    def crawl(self):
-        with webdriver_spider.Browser(self.url) as browser:
-            return self._crawl(browser)
-
-    def _crawl(self, browser):
-
-        data = {}
-
-        for rank in range(1, 11):
-            link_locator = "//div[@class='index block-group']/div[%d]/a" % rank
-            link_element = browser.find_element_by_xpath(link_locator)
-            link = link_element.get_attribute("href")
-
-            title_locator = "//div[@class='index block-group']/div[%d]/a/div/h3" % rank
-            title_element = browser.find_element_by_xpath(title_locator)
-            title = link_element.get_text()
-
-            data[rank] = {
-                "title": title,
-                "link": link,
-            }
-
-        return spider.CrawlResponseOk(data)
 
 if __name__ == "__main__":
     crawl_args = spider.CLICrawlArgs(HiddenObjectGamesSpider)
