0b803bee59d3258ee78b6ef87f7c076b4f402042
Dave Simons <simonsdave@gmail.com>
2015-11-22 09:23:09 -0500

    add spider for hiddenobjectgames.com

---
+++ gaming_spiders/hiddenobjectgames.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import json
+
+from cloudfeaster import spider
+from cloudfeaster import webdriver_spider
+
+
+class HiddenObjectGamesSpider(spider.Spider):
+
+    @classmethod
+    def get_metadata(cls):
+        return {
+            "url": "http://www.hiddenobjectgames.com/?sort=mostPlayed",
+        }
+
+    def crawl(self):
+        with webdriver_spider.Browser(self.url) as browser:
+            return self._crawl(browser)
+
+    def _crawl(self, browser):
+
+        data = {}
+
+        for rank in range(1, 11):
+            link_locator = "//div[@class='index block-group']/div[%d]/a" % rank
+            link_element = browser.find_element_by_xpath(link_locator)
+            link = link_element.get_attribute("href")
+
+            title_locator = "//div[@class='index block-group']/div[%d]/a/div/h3" % rank
+            title_element = browser.find_element_by_xpath(title_locator)
+            title = link_element.get_text()
+
+            data[rank] = {
+                "title": title,
+                "link": link,
+            }
+
+        return spider.CrawlResponseOk(data)
+
+if __name__ == "__main__":
+    crawl_args = spider.CLICrawlArgs(HiddenObjectGamesSpider)
+    crawler = spider.SpiderCrawler(HiddenObjectGamesSpider)
+    crawl_result = crawler.crawl(*crawl_args)
+    print json.dumps(crawl_result)
