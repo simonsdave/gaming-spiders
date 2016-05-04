c66be032ca5c0ab03be8ca3027c3c628fe0eb700
simonsdave <simonsdave@gmail.com>
2016-05-04 08:10:44 -0500

    refactor docker image builds

--- dockerfiles/build-docker-images.sh
+++ dockerfiles/build-docker-images.sh
@@ -5,35 +5,50 @@
 
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ "-v" == "${1:-}" ]; then
-    VERBOSE=1
-    shift
-else
-    VERBOSE=0
-fi
+VERBOSE=0
+TAG=""
 
-if [ $# != 4 ]; then
-    echo "usage: `basename $0` [-v] <email> <username> <password> <spiders-tar-gz>" >&2
+while true
+do
+    OPTION=`echo ${1:-} | awk '{print tolower($0)}'`
+    case "$OPTION" in
+        -v)
+            shift
+            VERBOSE=1
+            ;;
+        -t)
+            shift
+            TAG=${1:-}
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ $# != 2 ] && [ $# != 4 ]; then
+    echo "usage: `basename $0` [-v] [-t <tag>] <spiders-tar-gz> <username> [<email> <password>]" >&2
     exit 1
 fi
 
-EMAIL=${1:-}
+SPIDERS_TAR_GZ=${1:-}
 USERNAME=${2:-}
-PASSWORD=${3:-}
-SOURCE_SPIDERS_TAR_GZ=${4:-}
+EMAIL=${3:-}
+PASSWORD=${4:-}
 
-#
-# login to dockerhub
-#
-docker login --email="$EMAIL" --username="$USERNAME" --password="$PASSWORD"
-
-#
-# build services image
-#
-cp "$SOURCE_SPIDERS_TAR_GZ" "$SCRIPT_DIR_NAME/spiders.tar.gz"
 IMAGENAME=$USERNAME/gaming-spiders
+if [ "$TAG" != "" ]; then
+    IMAGENAME=$IMAGENAME:$TAG
+fi
+
+cp "$SPIDERS_TAR_GZ" "$SCRIPT_DIR_NAME/spiders.tar.gz"
 docker build -t $IMAGENAME "$SCRIPT_DIR_NAME"
 rm "$SCRIPT_DIR_NAME/spiders.tar.gz"
-docker push $IMAGENAME
+
+if [ "$EMAIL" != "" ]; then
+    docker login --email="$EMAIL" --username="$USERNAME" --password="$PASSWORD"
+    docker push $IMAGENAME
+fi
 
 exit 0
