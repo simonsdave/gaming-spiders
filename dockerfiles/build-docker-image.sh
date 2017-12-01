2d435f984c1062f1d3442dc7e8a12afd6c0f1a1f
simonsdave <simonsdave@gmail.com>
2017-12-01 08:29:31 -0500

    bug fix in build-docker-image.sh

--- dockerfiles/build-docker-image.sh
+++ dockerfiles/build-docker-image.sh
@@ -7,20 +7,21 @@ set -e
 
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ $# != 2 ] && [ $# != 3 ]; then
-    echo "usage: $(basename "$0") <spiders-tar-gz> <image-name> [<password>]" >&2
+if [ $# != 2 ] && [ $# != 4 ]; then
+    echo "usage: $(basename "$0") <spiders-tar-gz> <image-name> [<username> <password>]" >&2
     exit 1
 fi
 
 SPIDERS_TAR_GZ=${1:-}
 IMAGENAME=${2:-}
-PASSWORD=${3:-}
+USERNAME=${3:-}
+PASSWORD=${4:-}
 
 cp "$SPIDERS_TAR_GZ" "$SCRIPT_DIR_NAME/spiders.tar.gz"
 docker build -t "$IMAGENAME" "$SCRIPT_DIR_NAME"
 rm "$SCRIPT_DIR_NAME/spiders.tar.gz"
 
-if [ "$PASSWORD" != "" ]; then
+if [ "$USERNAME" != "" ] && [ "$PASSWORD" != "" ]; then
     docker login --username="$USERNAME" --password="$PASSWORD"
     docker push "$IMAGENAME"
 fi
