5881701cee99f356204a10003fc8aa45ef599f01
simonsdave <simonsdave@gmail.com>
2017-11-30 19:40:45 -0500

    refactoring build-docker-image.sh and .travis.yml

--- dockerfiles/build-docker-image.sh
+++ dockerfiles/build-docker-image.sh
@@ -7,38 +7,14 @@ set -e
 
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
-TAG=latest
-
-while true
-do
-    case "${1,,}" in
-        -t)
-            shift
-            # this script can be called by travis which may pass
-            # a zero length tag argument and hence the need for
-            # the if statement below
-            TAG=${1:-latest}
-
-            # the shift assumes the arg after the -t is always a
-            # tag name it just might be a zero length tag name
-            shift
-            ;;
-        *)
-            break
-            ;;
-    esac
-done
-
 if [ $# != 2 ] && [ $# != 3 ]; then
-    echo "usage: $(basename "$0") [-t <tag>] <spiders-tar-gz> <username> [<password>]" >&2
+    echo "usage: $(basename "$0") <spiders-tar-gz> <image-name> [<password>]" >&2
     exit 1
 fi
 
 SPIDERS_TAR_GZ=${1:-}
-USERNAME=${2:-}
+IMAGENAME=${2:-}
 PASSWORD=${3:-}
-
-IMAGENAME=$USERNAME/gaming-spiders:$TAG
 
 cp "$SPIDERS_TAR_GZ" "$SCRIPT_DIR_NAME/spiders.tar.gz"
 docker build -t "$IMAGENAME" "$SCRIPT_DIR_NAME"
