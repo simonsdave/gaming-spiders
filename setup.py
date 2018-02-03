53dd3e7ebdc58f05a21ee759d423d88286f5aa91
simonsdave <simonsdave@gmail.com>
2018-02-02 21:07:20 -0500

    cloudfeaster 0.9.8 -> 0.9.9

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     scripts=[
     ],
     install_requires=[
-        'cloudfeaster==0.9.8',
+        'cloudfeaster==0.9.9',
     ],
     version=version,
     description='Gaming Spiders',
