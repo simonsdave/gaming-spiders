c363c41c22da45ec38089ea93275dbe0de050af4
simonsdave <simonsdave@gmail.com>
2018-02-06 15:01:24 -0500

    start refactoring docker image build & push

--- dockerfiles/build-docker-image.sh
+++ dockerfiles/build-docker-image.sh
@@ -7,23 +7,16 @@ set -e
 
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ $# != 2 ] && [ $# != 4 ]; then
-    echo "usage: $(basename "$0") <spiders-tar-gz> <image-name> [<username> <password>]" >&2
+if [ $# != 2 ]; then
+    echo "usage: $(basename "$0") <spiders-tar-gz> <image-name>" >&2
     exit 1
 fi
 
 SPIDERS_TAR_GZ=${1:-}
-IMAGENAME=${2:-}
-USERNAME=${3:-}
-PASSWORD=${4:-}
+IMAGE_NAME=${2:-}
 
 cp "$SPIDERS_TAR_GZ" "$SCRIPT_DIR_NAME/spiders.tar.gz"
-docker build -t "$IMAGENAME" "$SCRIPT_DIR_NAME"
+docker build -t "$IMAGE_NAME" "$SCRIPT_DIR_NAME"
 rm "$SCRIPT_DIR_NAME/spiders.tar.gz"
-
-if [ "$USERNAME" != "" ] && [ "$PASSWORD" != "" ]; then
-    docker login --username="$USERNAME" --password="$PASSWORD"
-    docker push "$IMAGENAME"
-fi
 
 exit 0
