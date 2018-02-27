b7aad9c703d5228f6e36bf7d94c765592ee4df76
simonsdave <simonsdave@gmail.com>
2018-02-26 21:36:04 -0500

    fixing some shell script linting

--- .prep-for-release-master-branch-changes.sh
+++ .prep-for-release-master-branch-changes.sh
@@ -12,15 +12,17 @@ if [ $# != 0 ]; then
 fi
 
 REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
-REPO=$(basename "$REPO_ROOT_DIR")
+REPO=$(basename "${REPO_ROOT_DIR}")
 INIT_DOT_PY=$REPO_ROOT_DIR/${REPO//-/_}/__init__.py
-CURRENT_VERSION=$(grep __version__ "$INIT_DOT_PY" | sed -e "s|^.*=\\s*['\"]||g" | sed -e "s|['\"].*$||g")
+CURRENT_VERSION=$(grep __version__ "${INIT_DOT_PY}" | sed -e "s|^.*=\\s*['\"]||g" | sed -e "s|['\"].*$||g")
 
 # the pip install below is necessary make it super simple
 # to figure out the project's next version
 pip install semantic-version > /dev/null
 
-NEXT_VERSION=$(python -c "import semantic_version; print semantic_version.Version('$CURRENT_VERSION').next_minor()")
+NEXT_VERSION=$(python -c "import semantic_version; print semantic_version.Version('${CURRENT_VERSION}').next_minor()")
+
+sed -i -e "s|^\\s*__version__\\s*=\\s*['\"]${CURRENT_VERSION}['\"]\\s*$|__version__ = '${NEXT_VERSION}'|g" "${INIT_DOT_PY}"
 
 sed -i -e "s|^\\s*__version__\\s*=\\s*['\"]$CURRENT_VERSION['\"]\\s*$|__version__ = '$NEXT_VERSION'|g" "$INIT_DOT_PY"
 
