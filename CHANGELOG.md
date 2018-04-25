b0e5ff082d5182cd645f12d3c902f53c1b05b0a6
simonsdave <simonsdave@gmail.com>
2018-04-24 23:07:03 -0500

    cloudfeaster 0.9.12 -> 0.9.13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,7 +12,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ### Changed
 
-- cloudfeaster 0.9.10 -> 0.9.11
+- cloudfeaster 0.9.10 -> 0.9.13
 
 ### Removed
 
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     scripts=[
     ],
     install_requires=[
-        'cloudfeaster==0.9.12',
+        'cloudfeaster==0.9.13',
     ],
     version=version,
     description='Gaming Spiders',
