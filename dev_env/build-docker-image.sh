ec725eb7f2d2360f0fe2fbf2cebf61bef557a58f
Dave Simons <simonsdave@gmail.com>
2019-04-10 20:54:37 -0400

    refactor: simplify dev env docker build

--- dev_env/build-docker-image.sh
+++ dev_env/build-docker-image.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-#
-# This script builds a docker image which packages up the
-# development environment and associated tooling.
-#
 
 set -e
 
@@ -15,28 +11,23 @@ fi
 
 DOCKER_IMAGE=${1:-}
 
-CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
-PROJECT_HOME_DIR="$SCRIPT_DIR_NAME/.."
-
-cp "$PROJECT_HOME_DIR/requirements.txt" "$CONTEXT_DIR/."
-cp "$PROJECT_HOME_DIR/setup.py" "$CONTEXT_DIR/."
-mkdir "$CONTEXT_DIR/gaming_spiders"
-cp "$PROJECT_HOME_DIR/gaming_spiders/__init__.py" "$CONTEXT_DIR/gaming_spiders/."
-
-TEMP_DOCKERFILE=$CONTEXT_DIR/Dockerfile
+TEMP_DOCKERFILE=$(mktemp 2> /dev/null || mktemp -t DAS)
 cp "$SCRIPT_DIR_NAME/Dockerfile.template" "$TEMP_DOCKERFILE"
 
-CLF_VERSION=$(grep cloudfeaster== "$PROJECT_HOME_DIR/setup.py" | sed -e "s|^[[:space:]]*['\"]cloudfeaster==||g" | sed -e "s|['\"].*$||g")
+CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
+pushd "${SCRIPT_DIR_NAME}/.." > /dev/null && tar cf "${CONTEXT_DIR}/package.tar" . && popd > /dev/null
+
+CLF_VERSION=$(grep cloudfeaster== "${SCRIPT_DIR_NAME}/../setup.py" | sed -e "s|^[[:space:]]*['\"]cloudfeaster==||g" | sed -e "s|['\"].*$||g")
 sed \
     -i '' \
-    -e "s|%CLF_VERSION%|v$CLF_VERSION|g" \
-    "$TEMP_DOCKERFILE"
+    -e "s|%CLF_VERSION%|v${CLF_VERSION}|g" \
+    "${TEMP_DOCKERFILE}"
 
 docker build \
-    -t "$DOCKER_IMAGE" \
-    --file "$TEMP_DOCKERFILE" \
-    "$CONTEXT_DIR"
+    -t "${DOCKER_IMAGE}" \
+    --file "${TEMP_DOCKERFILE}" \
+    "${CONTEXT_DIR}"
 
-rm -rf "$CONTEXT_DIR"
+rm -rf "${CONTEXT_DIR}"
 
 exit 0
