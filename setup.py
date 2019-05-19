02dd2cb0ccd073d4ab601d31e4092524be2ae25d
Dave Simons <simonsdave@gmail.com>
2019-05-19 09:02:33 -0400

    feat: cloudfeaster 0.9.17 -> 0.9.18

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     scripts=[
     ],
     install_requires=[
-        'cloudfeaster==0.9.17',
+        'cloudfeaster==0.9.18',
     ],
     version=version,
     description='Gaming Spiders',
