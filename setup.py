099a4351f9b16e823e91e7ac23f336f08e776a52
Dave Simons <simonsdave@gmail.com>
2019-06-23 19:15:22 -0400

    feat: cloudfeaster -> v0.9.27

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'gaming_spiders/msnonlinegames.py',
     ],
     install_requires=[
-        'cloudfeaster==0.9.25',
+        'cloudfeaster==0.9.27',
     ],
     version=version,
     description='Gaming Spiders',
