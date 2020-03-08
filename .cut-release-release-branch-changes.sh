e1f304333842e628706ca32d86e8e9d84065c758
Dave Simons <simonsdave@gmail.com>
2020-03-08 10:53:22 -0400

    fix: .cut-release-release-branch-changes.sh

--- .cut-release-release-branch-changes.sh
+++ .cut-release-release-branch-changes.sh
@@ -11,11 +11,15 @@ fi
 
 RELEASE_BRANCH=${1:-}
 
+REPO_ROOT_DIR=$(repo-root-dir.sh)
+
+# README.md -------------------------------------------------------------------
+
 # requires.io
 sed -i "" \
     -e \
-    "s|?branch=master|?branch=$RELEASE_BRANCH|g" \
-    "$SCRIPT_DIR_NAME/README.md"
+    "s|?branch=master|?branch=${RELEASE_BRANCH}|g" \
+    "${SCRIPT_DIR_NAME}/README.md"
 
 # CodeFactor
 sed -i '' \
@@ -28,10 +32,14 @@ sed -i '' \
     "s|/overview/master|/overview/${RELEASE_BRANCH}|g" \
     "${REPO_ROOT_DIR}/README.md"
 
-# internal docs
-sed -i "" \
-    -e \
-    "s|(docs|(https://github.com/simonsdave/gaming-spiders/tree/$RELEASE_BRANCH/docs|g" \
-    "$SCRIPT_DIR_NAME/README.md"
+# -----------------------------------------------------------------------------
+
+rm -f "${REPO_ROOT_DIR}/README.rst"
+build-readme-dot-rst.sh
+
+rm -rf "${REPO_ROOT_DIR}/dist"
+build-python-package.sh
+
+# -----------------------------------------------------------------------------
 
 exit 0
