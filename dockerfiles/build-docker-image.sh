5f29be26c3a0920dec6319d48ee2926232e0e850
Dave Simons <simonsdave@gmail.com>
2020-11-15 19:36:42 -0500

    feat: 1st rev of package spiders in docker image

---
+++ dockerfiles/build-docker-image.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ $# != 2 ]; then
+    echo "usage: $(basename "$0") <package> <image-name>" >&2
+    exit 1
+fi
+
+PACKAGE=${1:-}
+IMAGE_NAME=${2:-}
+
+CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
+
+cp "${PACKAGE}" "${CONTEXT_DIR}/package.tar.gz"
+
+CLF_VERSION=$(grep cloudfeaster== "$(repo-root-dir.sh)/setup.py" | sed -e "s|^[[:space:]]*['\"]cloudfeaster==||g" | sed -e "s|['\"].*$||g")
+TEMP_DOCKERFILE=$(mktemp 2> /dev/null || mktemp -t DAS)
+sed \
+    -e "s|%CLF_VERSION%|${CLF_VERSION}|g" \
+    < "${SCRIPT_DIR_NAME}/Dockerfile.template" \
+    > "${TEMP_DOCKERFILE}"
+
+docker build \
+    -t "${IMAGE_NAME}" \
+    --file "${TEMP_DOCKERFILE}" \
+    "${CONTEXT_DIR}"
+
+rm -rf "${CONTEXT_DIR}"
+
+exit 0
