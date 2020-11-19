2c4421333cf9b54ef7d184c4a05add13ce27bf50
Dave Simons <simonsdave@gmail.com>
2020-11-18 22:58:10 -0500

    feat: bin/spiders.py now keyed by spider file name

--- bin/spiders.py
+++ bin/spiders.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 
+import importlib
 import json
+import os
 import sys
 
 from cloudfeaster.spider import SpiderDiscovery
@@ -9,6 +11,12 @@ from cloudfeaster.spider import SpiderDiscovery
 
 if __name__ == "__main__":
     sd = SpiderDiscovery()
-    print(json.dumps(sd.discover()))
+    metadata = sd.discover()
+    updated_metadata = {}
+    for spider_name in metadata.keys():
+        spider_module_name = '.'.join(spider_name.split('.')[:-1])
+        spider_module = importlib.import_module(spider_module_name)
+        updated_metadata[os.path.basename(spider_module.__file__)] = metadata[spider_name]
+    print(json.dumps(updated_metadata))
 
     sys.exit(0)
